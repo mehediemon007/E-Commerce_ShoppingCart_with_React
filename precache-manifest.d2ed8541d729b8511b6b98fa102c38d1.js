53c5e08cd9612d71885a0f8c144ef30ecd4e71a6
mehedi emon <mehediemon7@gmail.com>
2020-10-09 04:22:11 +0600

    Updates

---
+++ precache-manifest.d2ed8541d729b8511b6b98fa102c38d1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e0c79887e418ee75e02ae7ed8e620a",
+    "url": "/E-Commerce_ShoppingCart_with_React/index.html"
+  },
+  {
+    "revision": "fd75ded1f43606a7e258",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/css/main.1edcbc02.chunk.css"
+  },
+  {
+    "revision": "cb333426ba7bee524237",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/js/2.d377eb9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/js/2.d377eb9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd75ded1f43606a7e258",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/js/main.1a114a32.chunk.js"
+  },
+  {
+    "revision": "06b775544051c494ebba",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/js/runtime-main.a70cf67c.js"
+  },
+  {
+    "revision": "0210e597f574b4bd3f751ca6b3629f1c",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/media/bbb.0210e597.jpg"
+  },
+  {
+    "revision": "59731bae1af505b797e0bab856268623",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/media/dslr.59731bae.jpg"
+  },
+  {
+    "revision": "8d318e4a591da8b33dddbe1467a809c9",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/media/iphone.8d318e4a.jpg"
+  },
+  {
+    "revision": "e7bdef88d0ff67525ebeb96877270e62",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/media/microphone.e7bdef88.jpg"
+  },
+  {
+    "revision": "92ef6ab7b31320d776128b8bf7944473",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/media/perfume.92ef6ab7.jpg"
+  },
+  {
+    "revision": "986af3717a208f0b993fffa0d353abe6",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/media/rings.986af371.jpg"
+  },
+  {
+    "revision": "f30fe0b2e59c7281ecf5bd07d202a595",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/media/shoes.f30fe0b2.jpg"
+  },
+  {
+    "revision": "b4e58e91e391491d2836ce808e677037",
+    "url": "/E-Commerce_ShoppingCart_with_React/static/media/watch.b4e58e91.jpg"
+  }
+]);
