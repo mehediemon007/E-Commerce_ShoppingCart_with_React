035d10ddbebff985c992be259929f3be7f1ae49f
mehedi emon <mehediemon7@gmail.com>
2020-10-09 04:42:21 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Router>
             <Navbar />
             <Switch>
-              <Route path="https://mehediemon007.github.io/E-Commerce_ShoppingCart_with_React/" exact component={Product} />
+              <Route path="/E-Commerce_ShoppingCart_with_React/" exact component={Product} />
               <Route path="/cart" exact component={Cart} />
               <Route component={NotFound} />
             </Switch>
