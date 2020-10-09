b2c1502dabfc224ebbdbad73bca08778a88b84d8
mehedi emon <mehediemon7@gmail.com>
2020-10-09 11:29:30 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import ProductsContextProvider from "./Global/ProductsContext";
 import Product from "./components/Product";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route,HashRouter } from "react-router-dom";
 import Cart from "./components/Cart";
 import NotFound from "./components/NotFound";
 import CartContextProvider from "./Global/CartContext";
@@ -13,14 +13,14 @@ function App() {
     <div>
       <ProductsContextProvider>
         <CartContextProvider>
-          <Router>
+          <HashRouter basename="/">
             <Navbar />
             <Switch>
               <Route path="/" exact component={Product} />
               <Route path="/cart" exact component={Cart} />
               <Route component={NotFound} />
             </Switch>
-          </Router>
+          <HashRouter>
         </CartContextProvider>
       </ProductsContextProvider>
     </div>
